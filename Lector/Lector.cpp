aeed65abcb784ce7e5509f5aa2e6c74cb297ec2d
Esteban Campos <estebandcg1999@gmail.com>
2018-03-16 00:56:48 -0600

    Pruebas con Buffers

---
+++ Lector/Lector.cpp
@@ -0,0 +1,7 @@
+//
+// Created by estebandcg1999 on 15/03/18.
+//
+
+#include "Lector.h"
+
+
---
+++ Lector/Lector.h
@@ -0,0 +1,14 @@
+//
+// Created by estebandcg1999 on 15/03/18.
+//
+
+#ifndef TAREACORTA_LECTOR_H
+#define TAREACORTA_LECTOR_H
+
+
+class Lector {
+
+};
+
+
+#endif //TAREACORTA_LECTOR_H
